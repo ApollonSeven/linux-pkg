8e5cb5fecb105961e1192f7c64055e3f76634d26
Apollon Linux <apollonseven@gmail.com>
2020-08-02 04:03:51 +0300

    Add React Native
    - add RN
    - remove Balena Etcher
    - add command "echo"

--- script.sh
+++ script.sh
@@ -1,30 +1,37 @@
 #Packages
+echo "Install Packages (neofetch, curl, git)"
 sudo apt install neofetch curl git
 
 #NVM
+echo "Install NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 #Yarn
+echo "Install Yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 
 #Node
-nvm install 12.18.2
+echo "Install Node"
+nvm install --lts
 
 #Expo
+echo "Install Expo"
 npm install expo-cli --global
 
-#Balena Etcher
-echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-sudo apt update && sudo apt install balena-etcher-electron
+#React Native
+echo "Install React Native"
+npm install react-native-cli --global
+npm install create-react-native-app --global
 
 #Add Apt Repository
+echo "Install Software Properties Common"
 sudo apt-get install software-properties-common
 sudo apt update
 
 #Doom
+echo "Install Doom"
 sudo apt install prboom-plus doom-wad-shareware doomsday
